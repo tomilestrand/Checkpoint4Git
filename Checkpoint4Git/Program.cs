a171a3b33aef52863675f68f575deb8005bccfe6
Tom Ilestrand <tomilestrand@gmail.com>
2018-11-02 14:57:14 +0100

    Master of Impact

--- Checkpoint4Git/Program.cs
+++ Checkpoint4Git/Program.cs
@@ -7,9 +7,13 @@ namespace Checkpoint4Git
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!\nBestmovies ever!");
+<<<<<<< HEAD
             Console.WriteLine("Master of Impact);
             Console.WriteLine("Sudden Extremism");
             Console.WriteLine("Quadruple Humiliation");
+=======
+            Console.WriteLine("Master of Impact");
+>>>>>>> Master of Impact
         }
     }
 }
