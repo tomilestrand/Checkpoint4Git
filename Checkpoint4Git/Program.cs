0bd627aecaa91550b00aef497bb38124ff8a75e0
Tom Ilestrand <tomilestrand@gmail.com>
2018-11-02 14:58:41 +0100

    Infinite Extermination

--- Checkpoint4Git/Program.cs
+++ Checkpoint4Git/Program.cs
@@ -8,6 +8,7 @@ namespace Checkpoint4Git
         {
             Console.WriteLine("Hello World!\nBestmovies ever!");
             Console.WriteLine("Inferno of Action");
+            Console.WriteLine("Infinite Extermination");
             Console.WriteLine("Master of Impact");
             Console.WriteLine("Sudden Extremism");
             Console.WriteLine("Quadruple Humiliation");
