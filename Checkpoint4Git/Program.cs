f573ff951f78fdfd8b0695367c27ecb388b27d6f
Tom Ilestrand <tomilestrand@gmail.com>
2018-11-02 14:56:33 +0100

    Quadruple Humiliation

--- Checkpoint4Git/Program.cs
+++ Checkpoint4Git/Program.cs
@@ -7,8 +7,12 @@ namespace Checkpoint4Git
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!\nBestmovies ever!");
+<<<<<<< HEAD
             Console.WriteLine("Infinite Extermination");
             Console.WriteLine("Master of Impact");
+=======
+            Console.WriteLine("Master of Impact);
+>>>>>>> Quadruple Humiliation
             Console.WriteLine("Sudden Extremism");
             Console.WriteLine("Quadruple Humiliation");
         }
