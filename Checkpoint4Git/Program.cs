093a7b4cd108746bdbe167968ac5b9335b036fa4
Tom Ilestrand <tomilestrand@gmail.com>
2018-11-02 14:49:33 +0100

    Best movies ever!

--- Checkpoint4Git/Program.cs
+++ Checkpoint4Git/Program.cs
@@ -6,7 +6,7 @@ namespace Checkpoint4Git
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!\nBestmovies ever!");
         }
     }
 }
